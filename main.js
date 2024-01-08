c4978884b314afd51fb3929411a80efd58b77446
johnjoepreydangkulos@gmail.com <johnjoepreydangkulos@gmail.com>
2024-01-09 00:26:39 +0800

    added js to check for equal passwords

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log('Hello World!');
+const form = document.querySelector("form");
+const firstPass = document.querySelector(".first-pass");
+const confirmPass = document.querySelector(".confirm-pass");
+const invalidMsg = document.querySelector(".c-pass-invalid-msg");
+const btn = document.querySelector("input[type='submit']")
+
+confirmPass.addEventListener("input", (e) => {
+  if(firstPass.value != e.target.value){
+    confirmPass.classList.add("is-invalid");
+  } else if (firstPass.value == e.target.value){
+    confirmPass.classList.remove("is-invalid");
+  }
+  
+    
+});
+
+btn.addEventListener("click", (e) => {
+  if(!form.checkValidity()){
+    e.preventDefault();
+  }
+  
+  form.classList.add("was-validated");
+});
