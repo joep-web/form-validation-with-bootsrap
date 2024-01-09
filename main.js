771174d6fe95c97c7bdb7ce84d1486aa6306c367
johnjoepreydangkulos@gmail.com <johnjoepreydangkulos@gmail.com>
2024-01-09 17:51:50 +0800

    finished mobile view and password validation

--- main.js
+++ main.js
@@ -1,23 +1,34 @@
 const form = document.querySelector("form");
 const firstPass = document.querySelector(".first-pass");
 const confirmPass = document.querySelector(".confirm-pass");
-const invalidMsg = document.querySelector(".c-pass-invalid-msg");
-const btn = document.querySelector("input[type='submit']")
+const cPassInvalidMsg = document.querySelector(".c-pass-invalid-msg");
+const passInvalidMsg = document.querySelector(".pass-inv-msg");
+const btn = document.querySelector("input[type='submit']");
+
+firstPass.addEventListener("input", (e) => {
+  if(e.target.value == "") {
+    passInvalidMsg.innerText = ""
+  } else {
+    passInvalidMsg.innerText = "Less than 5 characters!"
+  }
+});
 
 confirmPass.addEventListener("input", (e) => {
   if(firstPass.value != e.target.value){
     confirmPass.classList.add("is-invalid");
-  } else if (firstPass.value == e.target.value){
+    cPassInvalidMsg.innerText = "Passwords not equal";
+  } else {
     confirmPass.classList.remove("is-invalid");
-  }
+    cPassInvalidMsg.innerText = "";
+  };
   
-    
+  confirmPass.pattern = `${firstPass.value}`;
 });
 
 btn.addEventListener("click", (e) => {
   if(!form.checkValidity()){
     e.preventDefault();
+    confirmPass.value == ""? cPassInvalidMsg.innerText = "": null;
   }
-  
   form.classList.add("was-validated");
 });
